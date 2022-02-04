2dc82d6a84b96a7e3d15ce31e38c1297895ea11a
byjusmx <87218747+byjusmx@users.noreply.github.com>
2022-02-04 15:30:13 -0600

    Update organise.py

--- organise.py
+++ organise.py
@@ -9,7 +9,7 @@ to_dir = "C:/WhiteHatJr/dowanloadedimages"
 list_of_files = os.listdir(from_dir)
 #print(list_of_files)
 
-# Move All Image files from Downloads Folder to Another Folder
+# Mueve todos los archivos tipo imagen de la carpeta Descargas a una nueva carpeta
 for file_name in list_of_files:
 
     name, extension = os.path.splitext(file_name)
@@ -20,21 +20,21 @@ for file_name in list_of_files:
         continue
     if extension in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
 
-        path1 = from_dir + '/' + file_name                       # Example path1 : Downloads/ImageName1.jpg        
-        path2 = to_dir + '/' + "Image_Files"                     # Example path2 : D:/My Files/Image_Files      
-        path3 = to_dir + '/' + "Image_Files" + '/' + file_name   # Example path3 : D:/My Files/Image_Files/ImageName1.jpg
+        path1 = from_dir + '/' + file_name                       # Ejemplo path1 : Descargas/NombreImagen1.jpg        
+        path2 = to_dir + '/' + "Image_Files"                     # Ejemplo path2 : D:/Mis archivos/Archivos_imagen      
+        path3 = to_dir + '/' + "Image_Files" + '/' + file_name   # Ejemplo path3 : D:/Mis archivos/Archivos_imagen/Nombreimagen1.jpg
         #print("path1 " , path1)
         #print("path3 ", path3)
 
-        # Check if Folder/Directory Path Exists Before Moving
-        # Else make a NEW Folder/Directory Then Move
+        # Checa si la ruta de la carpeta/directorio existen antes de moverlo
+        # Si no crear una nueva carpeta/directorio y muévelo
         if os.path.exists(path2):
-          print("Moving " + file_name + ".....")
+          print("Movviendo " + file_name + ".....")
 
-          # Move from path1 ---> path3
+          # Mueve de path1 ---> path3
           shutil.move(path1, path3)
 
         else:
           os.makedirs(path2)
-          print("Moving " + file_name + ".....")
+          print("Moviendo " + file_name + ".....")
           shutil.move(path1, path3)
